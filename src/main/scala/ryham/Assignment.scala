566cfccd40564898d10747f77f778992fc1b1f69
Joona Prehti <joona.prehti@student.tut.fi>
2018-10-12 19:01:28 +0300

    Task 2 almost ready, next to change days from 0-6 to 1-7

--- src/main/scala/ryham/Assignment.scala
+++ src/main/scala/ryham/Assignment.scala
@@ -13,6 +13,10 @@ import org.apache.spark.sql.types.DataType
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.{count, sum, min, max, asc, desc, udf, to_date, avg}
 import org.apache.spark.sql.functions.unix_timestamp
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.udf
+import scala.math._
 
 import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.functions.array
@@ -30,16 +34,59 @@ import org.apache.spark.rdd.RDD
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
+import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.VectorAssembler
+import scala.collection.immutable.Vector
+
+//import scala.reflect.internal.util.TableDef.Column
+
 object Assignment extends App {
   println("Assignment Main Starting ----------------------------------------")
+  
+  def calculateVx(dayInt:Double): Double= {
+	  return sin(2*Pi*dayInt/7)
+	}
+  val calculateVxUdf = udf(calculateVx _)
+  
+  def calculateVy(dayInt:Double): Double= {
+	  return cos(2*Pi*dayInt/7)
+	}
+  val calculateVyUdf = udf(calculateVy _)
+  
+  def convertDayToInt(Vkpv:String): Int = {
+    if (Vkpv == "Maanantai") {
+    	return 1
+		   
+    } else if(Vkpv == "Tiistai") {
+    	return 2
+    			
+    } else if(Vkpv == "Keskiviikko") {
+    	return 3
+    			
+    } else if(Vkpv == "Torstai") {
+    	return 4
+    			
+    } else if(Vkpv == "Perjantai") {
+    	return 5
+    			
+    } else if(Vkpv == "Lauantai") {
+    	return 6
+    			
+		} else if(Vkpv == "Sunnuntai") {
+		  return 7
+		   
+		} else {
+		   println("Day of the week not recognized")
+		   return 999
+		}
+  }
   
   def task1() = {
   	// X and Y coordinate columns are taken from trafficAccidentDataFrame df
     val data: DataFrame = trafficAccidentDataFrame.select("X","Y").limit(1000)
     
-    import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
-		import org.apache.spark.ml.linalg.Vectors
-		import org.apache.spark.ml.feature.VectorAssembler
+    
     
 		val vectorAssembler = new VectorAssembler()
 		.setInputCols(Array("X", "Y"))
@@ -48,7 +95,7 @@ object Assignment extends App {
 		import org.apache.spark.ml.Pipeline
 		val transformationPipeline = new Pipeline().setStages(Array(vectorAssembler))
 		
-		val pipeLine = transformationPipeline.fit(data)
+		val pipeLine = transformationPipeline.fit(data) // Error
 		val transformedTraining = pipeLine.transform(data)
 		transformedTraining.show
 		
@@ -73,9 +120,54 @@ object Assignment extends App {
 		}
    }
   
+  
+  // Notes explaining the Task 2
+  // We must divide the accidents on a unit circle, and calculate their x and y coordinates
+  // with SIN and COS. After this the data is ready to be used in 3 dimensions.
+  // Main problem in the adding of the weekday is that the weekdays monday and sunday for example
+  // are considered to be far away from each other, and in reality we want the boundary to be cyclic
+  // Meaning that also Monday and Sunday could be in the same cluster.
+  // x=sin(2pi*dayOfTheWeekInt/7),y=cos(2pi*dayOfTheWeekInt/7)
   def task2() = {
       println("Task2 starting")
-   }
+      println("Task Two Kohta 1 ----------------------------------------")
+      
+      // X and Y coordinate columns are taken from trafficAccidentDataFrame df
+	    val data: DataFrame = trafficAccidentDataFrame.select("X","Y","Vkpv").limit(100)
+	    
+			val turnWeekdayToInt = udf(convertDayToInt _)
+	    
+	    val dataWithDayInt = data.withColumn("dayInt", turnWeekdayToInt(col("Vkpv")))
+	    
+    	val dataWithVxCoordi = dataWithDayInt.withColumn("Vx", calculateVxUdf(col("dayInt")))
+	    val dataWithVyCoordi = dataWithVxCoordi.withColumn("Vy", calculateVyUdf(col("dayInt")))
+	    
+	    val vectorAssembler = new VectorAssembler()
+			.setInputCols(Array("X", "Y","Vx","Vy"))
+			.setOutputCol("features")
+			
+			import org.apache.spark.ml.Pipeline
+			val transformationPipeline = new Pipeline().setStages(Array(vectorAssembler))
+			
+			val pipeLine = transformationPipeline.fit(dataWithVyCoordi) // Error
+			val transformedTraining = pipeLine.transform(dataWithVyCoordi)
+			
+			val kmeans = new KMeans().setK(300).setSeed(1L)
+			
+			println("kohta 2")
+			
+			val kmModel = kmeans.fit(transformedTraining)
+			
+			println("kohta 3")
+			
+			val centers = kmModel.clusterCenters
+			
+//			centers.foreach(println)
+	   			
+			for (center <- centers) {
+				println(center(0) + "   " + center(1) + "   " + atan2(center(3), center(2)))
+			}
+		}
   
   def task3() = {
       println("Task3 starting")
@@ -111,19 +203,20 @@ object Assignment extends App {
 //  trafficAccidentDataFrame.printSchema()
   
   // Asks the user input in format "task n" where n is 1-6
-  val name = scala.io.StdIn.readLine().split(" ")
+//  val name = scala.io.StdIn.readLine().split(" ")
   
+  task2()
   // Launches the right function matching the
-  name(1).toInt match {
-    case 1  => task1()
-    case 2  => task2()
-    case 3  => task3()
-    case 4  => task4()
-    case 5  => task5()
-    case 6  => task6()
-    // catch the default with a variable so you can print it
-    case whoa  => println("Unexpected case: " + whoa.toString)
-}
+//  name(1).toInt match {
+//    case 1  => task1()
+//    case 2  => task2()
+//    case 3  => task3()
+//    case 4  => task4()
+//    case 5  => task5()
+//    case 6  => task6()
+//    // catch the default with a variable so you can print it
+//    case whoa  => println("Unexpected case: " + whoa.toString)
+//}
 
   println("Assignment Main Ending ------------------------------------------")
 }
